1bd368e46124c46e53caa216d5f9babe84616cae
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-04 21:00:03 +0100

    play with colors/pixels

--- day-19 Webcam Fun/scripts.js
+++ day-19 Webcam Fun/scripts.js
@@ -23,6 +23,16 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        //takes the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        //mess with the pixels
+        pixels = redEffect(pixels);
+        //pixels = rgbSplit(pixels);
+        //pixels = greenScreen(pixels);
+        ctx.globalAlpha = 0.8;
+        //put them back
+        ctx.putImageData(pixels, 0, 0)
+
     }, 16);
 }
 
@@ -38,8 +48,51 @@ function takePhoto() {
     link.setAttribute('download', 'handsome');
     link.innerHTML = `<img src="${data}" alt="Beautiful" />`;
     strip.insertBefore(link, strip.firstChild);
-
 }
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+    }
+    return pixels;
+}
+
+function rebSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //red
+        pixels.data[i + 100] = pixels.data[i + 1]; //green
+        pixels.data[i - 150] = pixels.data[i + 2]; //blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+    return pixels;
+  }
 
 getVideo();
 
