4c1cc371e4312ee9062ed772a251ee76599547ad
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-04 20:47:20 +0100

    day 19

---
+++ day-19 Webcam Fun/scripts.js
@@ -0,0 +1,46 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!`, err);
+        })
+}
+function paintToCanvas() {
+    const width = video.videWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    //play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Beautiful" />`;
+    strip.insertBefore(link, strip.firstChild);
+
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
